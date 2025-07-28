d8b701f4dbe543c02ea3ad648e5a70aaeb067cac
HasanCoder66 <hasanashraf874@gmail.com>
2025-07-28 20:44:20 +0500

    javascript book alert, variable string, variable numbers

---
+++ JavaScript/app.js
@@ -0,0 +1,30 @@
+// keyword  variable name ,   equals,   variable value
+var fullName;
+var lastName = "Ashraf";
+// alert(fullName);
+
+fullName = "Rafay";
+// alert(fullName);
+
+
+var number1 = 22;
+var number2 =    "22";
+
+console.log("my name is" + " " + fullName + " "  + lastName )
+
+console.log(number1 * number2)
+// 6546461
+// alert(lastName);
+// alert (fullName);
+// alert (fullName);
+// alert (fullName);
+// alert (fullName);
+// true
+
+// var fullName = "Hasan"
+
+// alert
+
+// reserved keyword  var, function
+
+// method  alert () map()
