20ede91d5feccc85d2b4a5b9327da07bf3918ba0
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-01 20:45:01 +0500

    chapter 3 to 6

---
+++ JavaScript/1-8-2025/app.js
@@ -0,0 +1,205 @@
+// alert("Welcome to my website");
+
+// 165461654514
+// true, false
+// "asdfjasldfj" 
+
+// var name = "hasan"
+// var fullName = "Hasan";           //declare
+// console.log("FullName =====>",fullName)
+          //declare
+
+          var num2 = 25;
+          var num1 = "25";
+
+          var newNum =   num2  + 25 + num1  + 25
+          console.log(newNum)
+// fullName = "Hasan"      //initialize
+// console.log(fullName)
+// fullName = "Asad"
+// console.log(fullName)
+
+
+
+// console.log(fullName)
+
+
+// special character $ _
+
+
+var $doll$ar = "$"
+// var _udersc ore = "_uderscore"
+// var +uderscore = "_uderscore"
+
+var response = "response"
+var myResponse = "myResponse"
+var myUserResponse = "myUserResponse"
+// console.log(_uderscore)
+
+var num = 40 ;
+// num--
+console.log(--num)
+// console.log(num)
+
+// var whatsLeftOver = 10 % 5;
+// console.log(num + 1)
+
+
+
+
+
+// chapter 1
+
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+// question 1
+alert("Hello World")
+
+
+// chapter 2 
+
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+// question 1
+var name = "hasan"
+
+
+
+
+
+// chapter 3
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+// question 1
+var num = 15
+
+
+
+// var fullName = ""; 
+
+// if(fullName == "") {
+//     alert("Fullname exist kerta hai")
+// }else {
+//     alert("full name nhi hai")
+// }
+// console.log("FullName =====>",fullName)
