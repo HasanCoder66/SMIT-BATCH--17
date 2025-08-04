a9a49e439917f371272634ebb7b0128a1e75cd2d
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-04 20:45:59 +0500

    chapter 7,8,9

---
+++ JavaScript/4-8-2025/app.js
@@ -0,0 +1,35 @@
+// var num = 5;
+
+// console.log(num++)
+// console.log(num)
+
+
+// var a =5;
+// var b = 2;
+
+// var c = ++a - --b + a++ + ++a
+        // 6   -  1  + 6
+// console.log(c)
+
+//  10 11 17 18
+
+// console.log(a--)  //  5
+// console.log(++a)  // 4 + 1 = 5
+
+// var a = 1 + (3 * 3)
+// var a = ((2 * 4) * 4) + 2;
+// var a = (2 * 4) * (4 + 2)
+
+// console.log(a) 
+
+// alert(23 + "23")
+// 
+var firstName = "Muhammad"
+var lastName = "Hasan"
+// alert(firstName + " " + lastName + "!")
+
+// var para = "ajlsdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkf"
+// console.log(firstName + " " + para + " " + lastName )
+
+var userInputEmail = prompt("Enter Your Email")
+console.log(userInputEmail)
