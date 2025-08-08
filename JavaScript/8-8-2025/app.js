729e6c1fa87b2477ed665c08c817de8190f7af05
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-08 20:15:22 +0500

    ch 12 to 14

---
+++ JavaScript/8-8-2025/app.js
@@ -0,0 +1,56 @@
+
+// keyword 
+
+// if (false) {
+//     // execution
+//     console.log("Hello, User")
+// } else {
+//     console.log("Please Sign In")
+// }
+
+
+
+
+// var lanaKiyaHai  = prompt("Kia mil raha ?")
+// var kg = 0.5;
+
+// if (lanaKiyaHai == "doodh" || kg == 1) {
+//     console.log("doodh le aou")
+// } else if(lanaKiyaHai == "bun") {
+//     console.log("aby yaar le aou")
+// }
+// else {
+//     console.log("kuch bi le aana pr khali na anaa")
+// }
+
+
+
+
+var lang = prompt("Enter Your Programming Language")
+
+
+
+// && exp == 2 && city == "Karachi"
+// if(lang == "JavaScript" ) {
+//     var exp = prompt("Enter Your Experience")
+//     if(exp == 2){
+// var city = prompt("Enter Your Location")
+// if(city == "Karachi") {
+//     console.log("Congratulation! You are Hired")
+// }else {
+//     console.log("Karachi ajaou")
+// }
+//     } else {
+//         console.log("jaou bary hoky aou ")
+//     }
+// // console.log("Hired")
+// } else {
+//     console.log("seekh ke aou")
+// }
+
+
+// if (lang != "JavaScript") {
+//     console.log("Hired")
+// } else {
+//     console.log("you are not elegible")
+// }
