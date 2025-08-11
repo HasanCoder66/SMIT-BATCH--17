acdb91d09d32f75b4c36ad5a7345ee6f3c2460f6
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-11 20:51:11 +0500

    revision if else and else if && || operators

---
+++ JavaScript/11-08-2025/app.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+// console.log("Hello World")
+
+// var userCountry = prompt("Enter Your Country");
+
+// console.log(typeof userAge);
+// console.log(typeof userCountry);
+
+// if (userCountry.toLowerCase() == "pakistan") {
+//   var userAge = +prompt("Enter Your Age");
+//   if (userAge >= 18) {
+//     var userVote = prompt("kis ko vote dena chahta ho");
+
+//     if (userVote == "Imran Khan") {
+//       var cnic = prompt("Enter your CNIC");
+
+//       if (cnic.length == 13) {
+//         console.log(typeof cnic)
+//         console.log("You Vote has been submitted");
+//       }
+//     } else {
+//         console.log("You are Elegible for Voting");
+//         console.log("Hum nahi le rahy  aap ka vote");
+//     }
+//   } else {
+//     console.log("baray ho kai aou");
+//   }
+// } else {
+//   console.log("Please Enter a Valid Input");
+// }
+
+
+var userInput = prompt ("Enter Your Age") || "User nai Age Nhi di"
+
+console.log(userInput)
