b2b58d05a44f8e5ffea09614f4156ab5d1d235e3
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-13 20:41:19 +0500

    ch 15, 16 & 17 Arrays

---
+++ JavaScript/13-08-2025/app.js
@@ -0,0 +1,28 @@
+console.log("Hello World!")
+
+// var city = "Karachi"
+
+var cities = ["Karachi", "Lahore", "Islamabad", "Punjab"];
+
+console.log(cities);
+
+// console.log(cities[cities.length , "Peshawar"])
+// var returnValue = cities.pop()
+
+// var returnValue = cities.shift()
+
+// console.log(returnValue)
+// var returnValue = cities.unshift("Quetta","Peshawar")
+
+//  var returnValue = cities.splice(1,3 , "Heyderabadh", "Multan")
+// console.log(returnValue)
+
+var hamza= cities.slice(1, 3)
+console.log(hamza)
+
+// alert(cities[0], cities[1])
+// console.log(cities.length -1)
+console.log(cities);
+
+// cities.slice()
+
