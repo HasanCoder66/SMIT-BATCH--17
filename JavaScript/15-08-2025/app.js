448744437b5677a4480d462ac01e7d19ad0c8f40
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-15 20:36:05 +0500

    chapter 15 to 17 revision

---
+++ JavaScript/15-08-2025/app.js
@@ -0,0 +1,28 @@
+// console.log("Hello World!");
+
+var student0 = "Hasan"
+var student1 = "Ashraf"
+
+
+// console.log("Student 0",student0)
+
+
+// var studentsArr = ["Hasan", "Ashraf", "Ayan", "Asad"];
+
+// var returnValue = studentsArr.splice(2,1,"Ali", "rafay")
+// console.log(returnValue)
+
+
+// var returnValue = studentsArr.slice(0, 2 )
+// console.log(returnValue)
+
+// console.log(studentsArr)
+// console.log(studentsArr.length)
+
+// studentsArr[studentsArr.length -1] = "Ahad"
+
+
+
+// var num = 3;
+
+// console.log()
