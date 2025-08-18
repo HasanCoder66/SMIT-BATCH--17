47272c75358c7c72b8daf9a246d98bf10633cbb3
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-18 20:49:28 +0500

    loops, flags, boolean, length, fifo, lifo, slice, splice, push, pop, shift, unshift

---
+++ JavaScript/18-08-2025/app.js
@@ -0,0 +1,103 @@
+// loop kia hai ? 
+// esa koi bi kaam jo baar baar repeat horaha ho usy loop kehty hain
+
+
+// for loop syntax 
+// for () {
+
+// }
+
+// statement        condition               iteration
+// start             end                    change
+// kahan sy chlna  // kahan tak chlna hai  // kesy chlna hai
+
+// ulta loop 
+// for (var i = 10; i >= 1; i--) {
+// console.log(i)
+// }
+
+// seedha loop 
+// for (var i = 1; i < 10; i++) {
+// console.log(i)
+// }
+
+
+// infinite loop 
+// for (var i = 1; i < 10;) {
+// console.log(i)
+// }
+
+
+
+
+
+
+
+var ytArr = ["video1", "video2", "video3"]
+
+// ytArr.push("Video 4")
+// ytArr.push("Video 5")
+// ytArr.push("Video 6")
+// ytArr.push("Video 7")
+// ytArr.push("Video 8")
+// ytArr.push("Video 9")
+// ytArr.push("Video 10")
+// ytArr.push("Video 11")
+
+// console.log(ytArr)
+
+// var flag = "no";
+
+// for (var i = 0; i < ytArr.length; i++) {
+    
+//     if(ytArr[i] == "video2") {
+//         flag = "yes";
+//         break
+//     }
+//     console.log(ytArr[i])
+
+// }
+
+
+
+// if(flag == "yes") {
+// console.log("video 2 milgaye")
+// } else {
+//     console.log("video no 2 nhi mili")
+// }
+
+
+
+
+// for(var i = 1; i < 5; i++) {
+//     if ( i == 2) {
+//         break
+//     }
+// console.log(i)
+// }
+
+
+
+
+
+
+// console.log("Hello World!")
+
+// var arr = ["string", true, 12, null, undefined];   //literal notaion
+// var newArr = new Array()  // object Notation
+
+
+// var multiDimensionalArr = [ ["ali", "hasan"], [1,2,3],[true, undefined, null]]
+
+// console.log(multiDimensionalArr[0][1])
+
+
+// arr[arr.length -1] = "ali"
+// console.log(arr[6])
+
+
+// var returnValue = arr.splice(1,3)
+// console.log(arr)
+// console.log(returnValue)
+
+
