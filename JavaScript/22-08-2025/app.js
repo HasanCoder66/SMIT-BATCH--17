dad21179905ebfa6b5dfd0b321b43b5014388582
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-22 20:43:34 +0500

    loops

---
+++ JavaScript/22-08-2025/app.js
@@ -0,0 +1,80 @@
+// console.log("Hello World!") ==>
+
+
+    // LOOP ==>
+
+// var flag = "no"
+
+// for (var i = 1; i <= 10; i++) {
+//     // if (i == 2) {
+//     //     flag == "yes"
+//     // }
+// console.log(i)
+// }
+
+
+// console.log(flag)
+
+
+
+// var arr = [1,2,3,4,5,46]
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+// for (var i = 10; i > 0; i--) {
+//     console.log(i)
+// }
+
+
+
+// for (var i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+
+
+
+// var numbers =[1,2,3,4,5,6]
+
+// var min = numbers[1]
+
+
+// for (var i = 0; i < numbers.length; i++) {
+//     // console.log(i)
+//     if(numbers[i] > min){
+//        min = numbers[i]
+//     }
+// }
+
+// console.log(min)
+
+
+
+var arr = [["Hasan", "Ashraf"], ["Rafay", "Ayan"], ["Ahsan", "Asad"]]
+var newArr = arr[0]
+
+// newArr[0] = ""
+// console.log(arr[0])
+// console.log(arr[])
+// console.log(arr)
+// console.log(arr.length)
+// for (var i = 0; i < arr.length; i++) {
+//     // console.log(arr[i])
+
+//     for (var j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j])
+//     }
+// }  
+
+
+
+
+for (var i = 10; i >= 0; i--) {
+    console.log(i)
+}
+
+
+
