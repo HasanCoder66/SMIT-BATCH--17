bc6840ebdcaceca61e8f46f01b6393f1b151fd71
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-25 20:48:49 +0500

    chapter 24 done

---
+++ JavaScript/25-08-2025/app.js
@@ -0,0 +1,63 @@
+// console.log("hello world!");
+
+// var arr = [["hasan","ashraf"], ["ayan", "saad"]];
+
+// console.log(arr[0][0])
+
+// console.log(arr)
+
+// for (var i = 0; i < arr.length; i++)  {
+//     // console.log(arr[i])
+
+//     for (var j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j])
+//     }
+// }
+
+// for (var i = 0; i < 5; i++)  {
+//     // console.log(arr[i])
+//     for (var j = 0; j < 5; j++) {
+//         console.log(i,j)
+//     }
+// }
+
+// var fullName = "muhamMad"
+
+// var firstCharacter = fullName.slice(0,1).toUpperCase()
+
+// var result = firstCharacter + fullName.slice(1).toLowerCase()
+
+// console.log(result)
+// console.log(fullName.toLowerCase())
+// console.log(fullName.length)
+
+var text =
+  "The New Yorker magazine doesnt allow the phrase  War II.  They say it should";
+
+  console.log(text.charAt(text.length -1))
+  console.log(text[text.length -1])
+
+// if (text.indexOf("abc") == -1) {
+//     console.log("Not Found")
+// } else {
+//     console.log("Found")
+// }
+
+// var flag = false;
+//   for (var i = 0; i <text.length; i++) {
+//     // console.log(text[i])
+
+//     // console.log(text.slice(i, i + 5))
+//     if (text.slice(i, i + 5).toLowerCase() == "world") {
+//         flag = true
+        
+//         break;
+//     }
+//   }
+
+
+//   if (flag) {
+//     console.log("World mil giya")
+//   } else {
+//     console.log("World nhi mila")
+//   }
