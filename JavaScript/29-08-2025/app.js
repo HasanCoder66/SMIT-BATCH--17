3f45d1b69d28c52485056142cb636ca9b9f0ceb1
HasanCoder66 <hasanashraf874@gmail.com>
2025-08-29 21:01:23 +0500

    Revision

---
+++ JavaScript/29-08-2025/app.js
@@ -0,0 +1,85 @@
+// console.log("Hello World!")
+
+
+// var firstName = "hasan";
+
+// console.log($Name_NAME)
+// =
+
+
+
+// var num = 1;
+
+// console.log(++num)
+// console.log(num--)
+// console.log(++num)
+
+
+// console.log(10 * 10 + (12 - 2))
+
+
+// var age = 16;
+
+// if (age >= 18){
+// console.log("You are elegible")
+// } else if (age == 16){
+
+//     console.log("2 saal bary hoky aou!")
+// }else {
+//     console.log("YOU ARE NOT ELEGIBLE!")
+// }
+
+
+// if (!true) {
+// console.log("hello")
+// } else {
+//     console.log("not hello")
+// }
+
+
+// var arr = []  //literal notaion
+// var arr = new Array()  //object notaion
+
+// var arr = [
+
+//     ["hasan",
+//          [ 1,]
+
+//         ]
+// ]  //literal notaion
+
+// console.log(arr[0][0])
+
+
+// for (var i = 0; i < arr.length; i++){
+//     console.log(arr[i][i])
+// }
+
+
+// var stuName = ["Rafay", "Ayan", "Saad","Asad"];
+
+// var score = [[398], [420], [490], [250]]
+
+// var totalScore = 500;
+
+// var percentage = [];
+
+// for (var i = 0; i < score.length; i++) {
+//     for(var j = 0 ; j < score[i].length; j++) {
+//         // console.log()
+//         if (score[i][j]) {
+//         percentage.push( score[i] / totalScore * 100)
+//         }
+//     }
+    
+//     // console.log(score[i])
+    
+// }
+
+// console.log(percentage)
+
+
+var firstName = "Hasan"
+firstName +=" Ashraf"
+
+console.log(firstName)
