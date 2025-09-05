948109daa79026ec170a5654a536fb6a1858703d
HasanCoder66 <hasanashraf874@gmail.com>
2025-09-05 20:34:11 +0500

    functions

---
+++ JavaScript/5-9-2025/app.js
@@ -0,0 +1,60 @@
+// console.log("Hello World!")
+
+// Ramadan Days
+// var ramdanDate = new Date("17 Feb 2026")
+// var currentDate = new Date();
+
+// var diff = ramdanDate.getTime() - currentDate.getTime()
+
+// var days = Math.round(diff / (1000 *60 *60 *24))
+// var month = Math.round(diff / (1000 *60 *60 *24 * 30))
+// console.log(month)
+// console.log(days)
+
+// console.log(ramdanDate.getTime())
+
+// method
+// alert("Hello world!")
+
+// Function
+
+// // parameter
+// function abc(a, b,c) {
+//   console.log(a,b,c );
+// }
+// var name = "Hasan"
+// abc(name); //arguements
+
+
+// function sum (num1, b) {
+//     console.log(num1 + b)
+// }
+
+// sum(12 , 5)
+// sum(15 , 5)
+// sum(120 , 5)
+// sum(1250 , 5)
+
+
+
+// function abc () {
+// console.log("Hello World")
+// }
+
+// var foo = ["hasan"];
+// // foo()
+
+// var returnValue = foo.splice(0)
+// console.log(returnValue)
+var plus ;
+
+function sum (a, b) {
+    // console.log(a + b)
+    var plus = a + b
+    return plus
+}
+
+// console.log(plus)
+console.log(sum(12, 67))
+// var returnValue = sum(12, 67)
+// console.log(returnValue)
