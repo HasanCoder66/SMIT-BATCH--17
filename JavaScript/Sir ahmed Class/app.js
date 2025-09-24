b375e7eb65c2ea4557fee8f6a6bdb45315a15229
HasanCoder66 <hasanashraf874@gmail.com>
2025-09-24 22:22:16 +0500

    sir ahmed class ch 21 to 25

---
+++ JavaScript/Sir ahmed Class/app.js
@@ -0,0 +1,56 @@
+// console.log("Hello world")
+
+// var arr = ["string", true]
+
+var myName = "hasan";
+// console.log(myName.charAt(myName.length -1))
+// console.log(myName[0])
+// console.log(myName.charAt(0))
+// for(var i =0; i <arr.length; i++){
+//     // console.log(arr[i])
+
+//     for (var j = 0; j < arr[i].length; j++){
+//         console.log(console.log(arr[i][j]))
+//     }
+// }
+
+// for (var i = 0; i < 5; i++){
+//     // console.log(i)
+
+//     for (var j = 0; j < 5; j++ ) {
+//         console.log(i,j)
+//     }
+// }
+
+// var text = "Hasan"
+// var text2 = "hasan"
+
+// var firstChar = text2.slice(0,1)
+// console.log(firstChar.toUpperCase())
+
+// var myName = firstChar.toUpperCase() + text2.slice(1,5)
+// console.log(myName.length)
+
+var para = "my name is hasan my father is name fulana my";
+
+console.log(para.replace(/my/g, "$"))
+// console.log(para.replace(/my/g, "$"))
+// console.log(para.indexOf("hasan"))
+// console.log(para.lastIndexOf("hasan"))
+// for (var i = 0; i < para.length; i++) {
+//   // console.log(para[i])
+//   // console.log(para.slice(i, i + 5 ) ) {
+//   if (para.slice(i, i + 5 == "Hasan")) {
+//     // para = para.slice(0,1)
+//    para =  para.replace("Hasan", "Ahsan")
+//   }
+// }
+// if(text.slice(i, i + 5))   {
+
+// }
+
+// console.log(para);
+
+// console.log(text.toUpperCase())
+
+// console.log(text.toLowerCase() == text2.toLowerCase())
