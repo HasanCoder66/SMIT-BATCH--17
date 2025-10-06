fb48fb546b65f5b854187299ab26e5ae232c01e0
hasancoder66 <hasanashraf874@gmail.com>
2025-10-06 20:33:37 +0500

    dom revision and slides 12 done

---
+++ JavaScript/06-10-2025/app.js
@@ -0,0 +1,42 @@
+console.log("Hello World!")
+// var allPara = document.getElementsByTagName("p")
+
+// console.log(allPara[0].innerHTML = "My Name is Hasan")
+
+// for(var i = 0; i < allPara.length; i++) {
+// allPara[i].style.color = "white" 
+// allPara[i].style.backgroundColor = "red" 
+// allPara[i].style.height = "100px" 
+// }
+
+
+// var para = document.getElementById("firstPara")
+// para.style.backgroundColor = "red"
+// para.style.color = "white" 
+// para.style.width = "200px" 
+// para.style.height = "200px" 
+// para.style.borderRadius = "50%" 
+
+
+
+// var  let const 
+
+
+// var console = console // global scope
+// console.log(console)
+
+
+let firstName = "Hasan"
+
+// firstName = "Ahsan"
+// console.log(firstName)
+
+
+// Es5
+console.log("My Name is" + " " + firstName)
+
+
+// Es6
+// Template Literals ==>
+var lastName = `${firstName} Ashraf` //  '' ``
+console.log(lastName)
