cccef4a1d35d7566ee0c8c0f0b1b8967ea8db4db
HasanCoder66 <hasanashraf874@gmail.com>
2025-10-17 22:56:50 +0500

    sir ahmed class code

---
+++ JavaScript/Sir ahmed Class/17-10-2025/Problem Solved/app.js
@@ -0,0 +1,59 @@
+// console.log("Hello World!")
+
+// function doNumlo(a, b) {
+//   // console.log(a, b)
+
+//   if (a == b) {
+//     var message = "Both are Same"
+//     return message ;
+//   }
+//   if (a > b) {
+//     console.log(a, " A is Greater tha B ", b);
+//   } else {
+//     console.log(b, " B is Greatet than A ", a);
+//   }
+// }
+
+// doNumlo(12, 12);
+
+
+
+
+
+
+
+// function sumofTwoArr ([a,b,c], [x,y,z]) {
+// let sumFirstArr = a + b +c
+// let sumSecArr = x + y +z
+
+// let result = sumFirstArr  - sumSecArr
+// console.log("Result -->",result)
+// }
+
+
+// sumofTwoArr([11,13, 5], [2,8,10])
+
+
+
+
+
+function generate8CharacterPass () {
+    var pass = "";
+    var mixChar = "sd@#jf74al;sfdoiq+_)(ejhv;jkhoqijtqw378"
+    // console.log("function is running --->")
+
+    for(let i =0; i < 8; i++){
+        // console.log(mixChar[28])
+        let random = Math.floor(Math.random()  * mixChar.length)
+        // console.log(random)
+
+        pass +=   mixChar[random]
+    }
+    console.log(pass)
+
+}
+
+
+
+generate8CharacterPass () 
+
