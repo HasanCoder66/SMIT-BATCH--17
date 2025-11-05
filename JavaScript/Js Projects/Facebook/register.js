8cd306108d1d94b23a5fca46f443697ce5b4ca8f
hasancoder66 <hasanashraf874@gmail.com>
2025-11-05 21:04:11 +0500

    registration complete logic

--- JavaScript/Js Projects/Facebook/register.js
+++ JavaScript/Js Projects/Facebook/register.js
@@ -1,4 +1,19 @@
-console.log("Running ->");
+// console.log("Running ->");
+
+function emptyFields(firstName, surName, email, password, date, month, year) {
+    console.log("empty wala chl raha hai -->")
+    document.querySelector("#firstName").value = ""
+    document.querySelector("#surName").value = "",
+        document.querySelector("#email").value = "",
+        document.querySelector("#password").value = ""
+        document.querySelector("#date").value = "",
+        document.querySelector("#month").value = "",
+        document.querySelector("#month").value = "",
+        month.value = "",
+        year = ""
+
+    console.log(firstName.value)
+}
 
 function signupHandler() {
     // console.log("mai signup hon chl raha hon")
@@ -10,46 +25,9 @@ function signupHandler() {
     let date = document.querySelector("#date").value
     let month = document.querySelector("#month").value
     let year = document.querySelector("#year").value
-
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     let userGender = ""
-    
-    if (firstName.trim() && surName.trim()) {
-        if (date && month && year) {
-            if (gender) {
-
-             }
-            else {
-                Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Please Select Your Gender!",
-                });
-            }
-        } else {
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "Please Enter Your Complete Date Of Birth!",
-            });
-        }
-    } else {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Please Enter Your Name Fields!",
-        });
-    }
-
-    let usersArr = localStorage.getItem("usersData") || []
-    // console.log("local storage sai data araha hai -->",usersArr)
-    let parseStorageData = JSON.parse(usersArr)
-
-    usersArr = parseStorageData
-    // console.log("FirstName -->",firstName)
-    // console.log("FirstName -->",firstName)
-    // console.log("FirstName -->",firstName)
-    // console.log("FirstName -->",firstName)
-    // console.log("FirstName -->",firstName)
+    let genderChecked;   // undefined
     let userData = {
         firstName: firstName,
         surName: surName,
@@ -59,25 +37,75 @@ function signupHandler() {
         dob: `${date}-${month}-${year}`
     }
 
-    usersArr.push(userData)
-    // console.log("Users Array -->",usersArr)
-    // console.log(userData)
-    // console.log("Date -->",date)
-    // console.log("Month -->",month)
-    // console.log("year -->",year)
-
-
-    // console.log(gender)
-
     for (let i = 0; i < gender.length; i++) {
         if (gender[i].checked) {
+            genderChecked = gender[i].checked
             userGender = gender[i].value
             userData.gender = userGender
         }
     }
+    if (firstName.trim() == "" || surName.trim() == "") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Please Enter Your Full Name!",
+        });
+        return
+    }
 
-    console.log(userGender)
+    if (date == "Date" || month == "Month" || year == "Year") {
+        Swal.fire({
+            icon: "error",
+            title: "Select your Dob",
+            text: "Please Enter Your Complete DOB!",
+        });
+        return
+    }
+
+    if (!genderChecked) {
+        // console.log(genderChecked)
+        Swal.fire({
+            icon: "error",
+            title: "Select Your Gender...",
+            text: "Please Enter Your Gender!",
+        });
+        return
+    }
+
+    if (password.length < 8) {
+        Swal.fire({
+            icon: "error",
+            title: "Week Password",
+            text: "Password sholud be at lease 8 characters!",
+        });
+    }
+
+    if (!emailPattern.test(email)) {
+        Swal.fire({
+            icon: "error",
+            title: "Invalid Email...",
+            text: "Please Enter a correct Email!",
+        });
+        return
+    }
+
+
+    let usersArr = localStorage.getItem("usersData") || []
+    // console.log("local storage sai data araha hai -->",usersArr)
+    let parseStorageData = JSON.parse(usersArr)
+
+    usersArr = parseStorageData
+
+    usersArr.push(userData)
+
     localStorage.setItem("usersData", JSON.stringify(usersArr))
-    // console.log(password)
-    // console.log(email)
+
+    Swal.fire({
+        icon: "success",
+        title: "Registration Completed!",
+        text: "Thank you for your signup!",
+    });
+
+    emptyFields(firstName, surName, email, password, date, month, year)
+
 }
