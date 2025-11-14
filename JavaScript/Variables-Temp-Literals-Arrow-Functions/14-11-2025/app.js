2f73d690994e81abb6e02c9a3805e01971fd9adb
hasanCoder66 <hasanashraf874@gmail.com>
2025-11-14 20:36:16 +0500

    Arrow Function | let vs const vs var | Template Literals

---
+++ JavaScript/Variables-Temp-Literals-Arrow-Functions/14-11-2025/app.js
@@ -0,0 +1,172 @@
+
+
+// // // let , const , var  
+
+// // // var a;          // declare
+// // // a = "a"         //initialize
+// // // var a = "a"  //declare and initialize
+
+// // // console.log(a)
+
+
+// // // var a = "a";
+
+// // // a = "b"
+
+
+// // // console.log(a)
+
+
+// // // strongly type      datatype:string  = variableName = value
+// // // loss type
+
+
+// // // var a = "12";
+
+
+
+// // // var age = 18;
+
+
+// // // age = "hasan";
+
+
+
+
+
+// // // let a = 1;
+// // // console.log(a)
+
+
+
+// // // {
+// // //     let a = 1
+// // // }
+
+// // // console.log(a)
+
+
+// // // var c = "c";
+
+
+
+
+
+// // // {
+// // //     var d = "d" 
+// // // }
+// // // console.log(d)
+
+// // // let num;
+
+// // // console.log(num)
+
+
+
+
+// // // let e = "e"
+
+
+// // // let e = "e"
+
+// // //  e = "c"
+
+// // const a = 0
+ 
+// // {
+// //     console.log(a)
+// // }
+
+// // // a = 1
+
+
+
+// // // console.log(a)
+
+
+// // var a = "abc";
+// // let c = "xyz"
+// // const foo = "foo"
+
+
+
+// // for(let i =0; i < 4 ; i++){
+// //     console.log(i)
+// // }
+
+
+// // const a = 12
+
+// // a = 15
+
+// // console.log(a)
+
+
+// // let lastName = "ashraf"
+// // let abc = `hasan ${lastName}`
+// // console.log(abc)
+
+
+// // let text = `my name is hasan 
+// //  ${btn.addEvent}
+// // `
+
+// // console.log()
+
+
+
+
+
+// // syntax , // hoisting , // arguements, // this keyword
+
+// // console.log(abc(12,13))
+
+// // function abc (a, b) {
+// // return a+b
+// // }
+
+
+
+// // function expression 
+
+// // var abc = function () {
+// // console.log(arguments)
+// // }
+
+
+
+// // abc(12, 45, 85,1)
+// // Arrow Functions -->
+
+// // let abc = (a,b,c,d) => {
+// //     // return a + b + c+d
+// //     console.log(arguments)
+// // }
+// // console.log(abc(12, 12, 525, 789,))
+
+
+// let obj = {
+//     firstName :"hasan",
+//     lastName :"ashraf",
+//     // getFullName : function () {
+//     //     // console.log(`${this.firstName} ${this.lastName}`)
+//     //     console.log(this)
+//     // }
+//     getFullName : () => {
+//         console.log(`${obj.firstName} ${obj.lastName}`)
+//         console.log(this)
+//     }
+// }
+
+// obj.getFullName()
+
+
+// const sum =  () => 12 + 12     // one liner
+
+
+// // const sum =  () => {
+// //     return  12 + 12
+// //   }           
+
+
+// console.log(sum())
