a4be9db4d0a03b5314abe94f5d9ecb348dc5a1eb
hasanCoder66 <hasanashraf874@gmail.com>
2025-11-21 20:55:08 +0500

    high order fn cb default params

---
+++ JavaScript/21-11-2025/app.js
@@ -0,0 +1,133 @@
+console.log("Js Running -->")
+
+
+// parameters 
+
+
+// Default Parameters
+// function fullName (a = "Hasan") {
+// console.log(a + "Ashraf")
+
+// }
+
+
+// fullName("Ahsan")   // arguement
+
+// function sum (n = 12, b = 45) {
+//     return n * b
+// }
+
+// console.log(sum())
+
+
+
+
+// const sum = (n = 12, n2 = 5) => n * n2   // one liner
+
+
+// console.log(sum())
+
+// let obj = {
+//     myFunc : () => {
+//         console.log("chl raha hon")
+//     }
+// }
+
+
+// obj.myFunc()
+
+
+// const arrow = () => {
+//     if (true) return "hasan"
+//     console.log("nhi jaraha hai")
+// }
+
+
+// console.log(arrow())
+
+
+
+
+// High Order Function kia hota hai ?
+
+
+// function highOrderFn (cb, n1, n2) {
+//     // console.log()
+//     cb(n1, n2)
+
+// }
+
+// function doosra (a, b) {
+//     console.log(a + b)
+// }
+
+// highOrderFn(doosra, 12 , 13)
+
+
+// const randomNumGenerator = () => {
+//     return Math.floor(Math.random() * 500)
+// }
+
+// const id = (name,cb) => {
+//    return name + "_" + cb()
+// } 
+
+// const userId = id("Hasan", randomNumGenerator)
+
+// console.log(userId)
+
+
+
+// const sum = () => 12  + 15
+
+
+// const highOd = (cb) => {
+// return cb()
+// }
+
+
+// console.log(highOd(sum))
+
+// fullName return a + " " + b
+// highOder(fn)  chala arguement fn
+// fn ("hasan", "ashraf")
+
+
+
+// let numbers = [1, 2,3,4,5,6,7,8]
+let stuName = ["hasan", "saleem", "shakoor"]
+
+let konkonsaielemmily = [];
+
+
+
+stuName.forEach((s) => console.log(s))
+
+// for(let i = 0; i<numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+
+// function myName (stuN) {
+//     console.log(stuN)
+
+//     konkonsaielemmily.push(stuN)
+    
+// }
+
+// console.log(konkonsaielemmily)
+
+
+// function foo () {
+//     myName("hasan")
+// }
+
+
+// foo()
+
+
+// function banadiya () {
+
+// }
+
+// console.log(banadiya())
