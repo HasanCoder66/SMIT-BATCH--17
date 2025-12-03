b5f26e583803596db0826b96ba860ccd2274f3c1
hasanCoder66 <hasanashraf874@gmail.com>
2025-12-03 21:13:49 +0500

    Revision and Constructor Or Objects Methods

---
+++ JavaScript/Constructor-Function-3-12-25/app.js
@@ -0,0 +1,95 @@
+console.log("Js Running -->")
+
+
+// Constructor kia hota hai
+
+// let student1 = {
+//     name : "hasan",
+//     email :"hasan@gmail.com",
+//     monitor : true,
+// }
+
+// let student2 = { name: "abdullah", email: "abdullah@gmail.com", isMonitor: false, }
+
+
+
+// console.log(student1.is)
+
+
+
+// function Student(name, email) {
+//     this.name = name
+//     this.email = email
+// }
+
+// let returnObj = new Normal("hasan")
+// let returnObj2 = new Normal("hasan", "hasan@gmail.com")
+
+// console.log(returnObj)
+// console.log(returnObj2)
+
+
+// let students =[]
+
+// let stu1 = new Student("stu1", "student1@gmail.com")
+// let stu2 = new Student("stu2", "student2@gmail.com")
+
+// console.log(stu2)
+
+// students.push(stu1, stu2)
+
+// console.log(students)
+
+
+// const arrow = () => {
+//     console.log(this)
+// }
+
+// arrow()
+
+
+
+// let objForExtra = {
+//     getFullName : function () {
+
+//     }
+// }
+
+
+
+// function Ayan () {
+//     this.name = "Ayaan"
+// }
+
+// let newObj = new Ayan()
+// let newObj5 = new Ayan()
+
+
+// Ayan.prototype.getFullName = function (firstName, lastName) {
+//     console.log(firstName + " " + lastName)
+// }
+
+// newObj5.getFullName("Hasan" , "Ashraf")
+
+
+
+// keys , entries , values, seal , freeze, hasOwn
+let obj = {
+    name :"hasan",
+    secondName : "ashraf",
+    age : 22,
+    email :"codermhasan@gmail.com"
+}
+// console.log(Object.keys(obj))
+// console.log(Object.entries(obj))
+// console.log(Object.values(obj))
+// console.log(Object.freeze(obj))
+// console.log(Object.seal(obj))
+
+// obj.isAdmin = true
+// obj.name = true
+
+// console.log(obj)
+
+
+// console.log(Object.hasOwn(obj,"invalid")) 
