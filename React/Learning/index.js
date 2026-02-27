f3779803a41c4515cf92e0343fb0659c0b518ad9
Hasan Coder 66 <hasanashraf874@gmail.com>
2026-02-27 22:34:49 +0500

    React 1st class

---
+++ React/Learning/index.js
@@ -0,0 +1,12 @@
+// let h1TagWithText = <h1 style={{}}>Hello </h1>
+
+const abc = () => {
+ return "12"   
+}
+
+
+//Named Export -->
+export {abc}
+
+// Default Export   -->  wo aap kisi bi name sai import kersekty ho 
+// export default abc
---
+++ React/Learning/topics.js
@@ -0,0 +1,122 @@
+// REACT JS INTRODUCTION || DIFF BETWEEN LIB & FRAMEWORK | SETUP PROJECT
+
+import {abc} from "./index.js"
+// const { default: abc } = require(".")
+
+// What is React JS ?
+// Difference Between Library & Framework
+// What is JSX ?
+// Import and Export
+// Setup React Project Using Vite
+
+
+
+// UI 
+
+// User Interfaces 
+
+
+// Market Trending Tool / lib
+// Complex Frontend
+
+
+// React JS SPA ==> index.html  -> jis mai saaraa kaam KIYA JAYga.
+// Approach ==> Component based architecture 
+
+
+// Ecomm --> add to cart --> badge --> val --> update --> reload --> function call kerna padta
+// javascript 
+
+
+// DOM => DOCUMENT OBJECT MODEL   ==> TREE
+
+// RELOAD  ==>  RENDER
+
+
+
+// REACT 
+
+
+
+
+
+
+// Difference Between Library & Framework
+
+
+// PACKAGES jinka use kerty hai hum koi kaam kerlen.
+
+// sweet alert  --> lib --> ye kaam alert ka kisi nai kerky rakha howa hai... 
+
+// download or iska use kerlo 
+
+// material ui --> kuch components --> 
+
+
+
+
+// RULES ==>    jo necessary hai follow kerna ==>
+
+    // Next Js 
+    // Angular 
+
+
+
+    // flat
+    // plot 
+
+    // 1 lac 
+
+
+
+
+    // React Js App  --> 
+
+    // multiple packages install 
+    // apny hisaab sai application
+
+    // requirement
+
+    // js api call fetch
+    // json prase 
+
+    // library
+    // axios 
+
+
+
+
+// What is JSX ?
+
+// html ==>{/* <h1> hello</h1> */}
+
+// let h1Tag = document.createElement("h1")
+// let h1Text = "Hello"
+
+// h1Tag.append(h1Text)
+
+
+{/* <h1>Hello</h1> */}
+
+// What is jsx
+
+// Jsx is a combination of HTML & Javascript 
+
+
+// console.log(abc())
+
+
+// Vite --> Bundler  --> React Project setup 
+
+// react --> slow
+
+
+
+// node  --> run time environment
+// npm --> node package manager 
+
+
+// vite cmd 
+// npm create vite 
+
+// npm run dev 
