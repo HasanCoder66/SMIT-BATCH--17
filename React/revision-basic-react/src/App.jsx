a06c033bbd0107ad930206993a2dd3558304f2d7
Hasan Coder 66 <hasanashraf874@gmail.com>
2026-03-18 00:12:59 +0500

    react revision from start last ashra extra class

---
+++ React/revision-basic-react/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+    <div className="flex justify-between">
+      <p className="w-[200px] h-[200px] rounded-2xl bg-amber-300 flex justify-center items-center font-bold text-red-700 text-2xl">1</p>
+      <p className="w-[200px] h-[200px] rounded-2xl bg-gray-300 flex justify-center items-center font-bold text-red-700 text-2xl">2</p>
+      <p className="w-[200px] h-[200px] rounded-2xl bg-blue-300 flex justify-center items-center font-bold text-red-700 text-2xl">3</p>
+    </div>
+    </>
+  )
+}
+
+
+export default App
