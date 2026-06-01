7998cc3f10287a85be5484eeed37102255cd7a00
hasanCoder66 <hasanashraf874@gmail.com>
2026-06-01 18:09:45 +0500

    node js slide

--- React/redux-02/src/App.jsx
+++ React/redux-02/src/App.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import Signup from './pages/Signup'
-import ProtectedRoute from './components/ProtectedRoute'
-import PublicRoute from './components/PublicRoute'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
 const App = () => {
   return (
     <Routes>
-
-      <Route path='/' element={<h1>Han jani</h1>} />
-      <Route path='/login' element={<PublicRoute><Login /> </PublicRoute>} />
-      <Route path='/signup' element={<PublicRoute><Signup /></PublicRoute>} />
-      <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-
+      <Route path="/" element={<h1>Han jani</h1>} />
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />{" "}
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <PublicRoute>
+            <Signup />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
