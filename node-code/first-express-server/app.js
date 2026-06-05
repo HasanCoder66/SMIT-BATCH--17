94fc9c73f86bcb1ce3ccc9a00336c324efa7ebbe
hasanCoder66 <hasanashraf874@gmail.com>
2026-06-05 20:25:42 +0500

    first express server

---
+++ node-code/first-express-server/app.js
@@ -0,0 +1,35 @@
+// console.log("js is running");
+
+
+const express = require("express")
+
+let users = [
+    {
+        name: "ayaan faheem",
+        age: 13,
+        email: "ayanfaheem@gmail.com"
+    },
+    {
+        name: "ayaan hashmi",
+        age: 18,
+        email: "ayanhashmi@gmail.com"
+    },
+]
+
+const app = express()
+
+app.get("/", (req, res) => {
+  
+    res.status(200).send("hello from the server.")
+})
+
+
+app.get("/users", (req, res) => {
+    res.status(200).send(users)
+})
+
+
+
+app.listen(5500, () => {
+    console.log(`server is listening on port:5500`);
+})
