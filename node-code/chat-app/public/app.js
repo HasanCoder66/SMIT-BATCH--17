0ea0508090d392ab157fc8677d4cb3df64ac8e2f
hasanCoder66 <athar.flutter.smit786@gmail.com>
2026-07-20 20:25:29 +0500

    chat app done

--- node-code/chat-app/public/app.js
+++ node-code/chat-app/public/app.js
@@ -2,30 +2,70 @@ const socket = io();
 // console.log("user connected...");
 
 
-const input = document.getElementById("messageInput")
+const chatUserName = document.getElementById("chat-userName")
 const sendBtn = document.getElementById("sendBtn")
+const input = document.getElementById("messageInput")
+const joinBtn = document.getElementById("joinBtn")
 const messagesArea  = document.getElementById("messages");
+const showOnlineUsers  = document.getElementById("show-online-users");
+const notify = document.getElementById("notify");
 
 
-socket.on("user-message", (data) => {
-    console.log(data);
+// server responds...
+
+socket.on("user-message", (message) => {
     const li = document.createElement("li")
-    li.innerHTML = data
+    li.innerHTML = message
     messagesArea.appendChild(li)
+})
 
-    // li.innerHTML = ""
-    // console.log(data);
+socket.on("online-users", (count) => {
+    showOnlineUsers.innerHTML = `Online Users : ${count}`
+})
+
+socket.on("joined-user", (user) => {
+    notify.innerHTML = `${user} joined chat`
+})
+
+socket.on("left-user", (leftUser) => {
+    notify.innerHTML = `${leftUser} left chat`
+})
+socket.on("typing-user", (typist) => {
+    notify.innerHTML = `${typist} is typing...`
+
+    setTimeout(() => {
+notify.innerHTML = `Notification`
+    },1500)
 })
 
 const sendMessage = () => {
     console.log("click how...", input.value);
 
+    if(chatUserName.value){
     socket.emit("message", input.value)
+    input.value = ""
+}else{
+    alert("please join chat first..")
+}
 
    
 
 
 }
 
+const joinUser = () => {
+    if(!chatUserName.value.trim()){
+        return alert("please enter a join chat name...")
+    }
+
+    socket.emit("join-user", chatUserName.value)
+}
+
 
 sendBtn.addEventListener("click", sendMessage)
+joinBtn.addEventListener("click", joinUser)
+input.addEventListener("keypress", () => {
+    console.log("key pressed");
+
+    socket.emit("typing", chatUserName.value)
+})
--- node-code/chat-app/server.js
+++ node-code/chat-app/server.js
@@ -7,6 +7,8 @@ app.use(express.static("public"))
 const server = http.createServer(app);
 
 const io = new Server(server)
+let users = [];
+
 
 io.on("connection", (socket) => {
     console.log(socket.id);
@@ -14,9 +16,40 @@ io.on("connection", (socket) => {
     socket.on("message", (data) => {
         console.log(data);
 
-        socket.emit("user-message", data)
+        io.emit("user-message", data)
         // io.emit("send-message-to-all", data)
     })
+
+    socket.on("join-user", (joinUserName) => {
+        users.push({
+            id: socket.id,
+            userName: joinUserName
+        });
+
+        io.emit("online-users", users.length)
+        io.emit("joined-user", joinUserName)
+    })
+
+
+    socket.on("disconnect", () => {
+
+        const findleftUser = users.find((user) => user.id === socket.id);
+
+        users = users.filter((user) => user.id !== socket.id)
+        io.emit("online-users", users.length)
+
+        if (findleftUser) {
+            io.emit("left-user", findleftUser.userName)
+        }
+
+
+    })
+
+    socket.on("typing", (typist) => {
+        socket.broadcast.emit("typing-user", typist)
+    })
+
+
 })
 
 server.listen(5500, () => {
